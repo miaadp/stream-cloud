3d329de1d6d0f2685047e6c9f285e4f1b5aea2e8
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2021-11-29 00:04:49 +0330

    Update router.py

--- api/router.py
+++ api/router.py
@@ -25,16 +25,18 @@ class Router:
         try:
             id = int(id_hex, 16)
         except ValueError:
-            return web.HTTPNotFound()
+            return web.Response(text='Insert hex number')
+
         message = await self.client.get_messages(self.CHANNEL, ids=id)
         if not message or not message.file:
-            return web.HTTPNotFound()
+            return web.Response(text='id not found or its not file message')
+
         file_ext = message.file.ext
         if file_ext in [".mp4", ".mkv"]:
             link = "{Config.DOMAIN}/player/{id}"
             return web.Response(text='<!DOCTYPE html><html lang=fa dir=rtl><head><meta charset=UTF-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width, initial-scale=1.0"><title>DBmovies player</title><link rel=stylesheet href=/player/css.css></head><body><div class=container-fluid><div class="container justify-content-center"><div class="mt-5 d-flex justify-content-center"><div class="row shadow p-3 mt-5 rounded justify-content-center"><div class="mb-3 d-flex justify-content-center"><video id=my-video class="video-js vjs-theme-forest" controls preload=metadata width=320 height=200 data-setup={}><source src="' + link + '" type=video/mp4></video></div><div class="mt-3 row"><div class="d-flex justify-content-center"><div class=mb-3><label for=formFile class="form-label text-white"> بخش افزودن زیرنویس</label><input class="form-control form-control-sm" type=file id=formFile name=fileItem accept=.srt,.vtt value=hiasfsa.srt><div class="mt-1 justify-content-center"><button class="btn btn-outline-light" type=button onclick=addSub()>آپلود</button></div></div></div></div></div></div></div></div><footer class="mt-3 text-white fixed-bottom" style=background-color:#000013><div class=container><div class=row><div class="mt-3 mt-2 d-flex justify-content-center"><p style=text-align:left;font-size:12px>Design by DBmovies</p></div><div class="d-flex justify-content-center"><p style=text-align:left;font-size:10px>All Rights Reserved. &copy 2021</p></div></div></div></footer></body><script src=/player/js.js></script></html>',content_type='text/html')
         else:
-            return web.HTTPNotFound()
+            return web.Response(text='message format is not mp4 or mkv , message format is '+file_ext)
 
     async def Downloader(self, request):
         id_hex = request.match_info.get("id")
@@ -42,12 +44,12 @@ class Router:
         try:
             id = int(id_hex, 16)
         except ValueError:
-            return web.HTTPNotFound()
+            return web.Response(text='Insert hex number')
 
         message = await self.client.get_messages(self.CHANNEL, ids=id)
 
         if not message or not message.file:
-            return web.HTTPNotFound()
+            return web.Response(text='id not found or its not file message')
 
         offset = request.headers.get("Range", 0)
 
@@ -97,4 +99,3 @@ class Router:
                 await resp.write(part)
 
         return resp
-    
