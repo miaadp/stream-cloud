978cda76f0729d65abebc27827bc2a46f23020e2
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2021-11-29 18:45:34 +0330

    Update router.py

--- api/router.py
+++ api/router.py
@@ -30,9 +30,11 @@ class Router:
         if file_ext in [".mp4", ".mpv", ".mkv"]:
             link = f"{Config.DOMAIN}/{id_hex}"
             name = Config.CHANNEL_USERNAME
-            return web.Response(text='<!DOCTYPE html><html lang=fa dir=rtl><head><meta charset=UTF-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width, initial-scale=1.0"><title>' + name + ' player</title><link rel=stylesheet href=https://miaadp.github.io/stream-cloud/style.css></head><body><div class=container-fluid><div class="container justify-content-center"><div class="mt-5 d-flex justify-content-center"><div class="row shadow p-3 mt-5 rounded justify-content-center"><div class="mb-3 d-flex justify-content-center"><video id=my-video class="video-js vjs-theme-forest" controls preload=metadata width=320 height=200 data-setup={}><source src="' + link + '" type=video/mp4></video></div><div class="mt-3 row"><div class="d-flex justify-content-center"><div class=mb-3><label for=formFile class="form-label text-white">بخش افزودن زیرنویس|Add subtitle</label><input class="form-control form-control-sm" type=file id=formFile name=fileItem accept=.srt,.vtt value=hiasfsa.srt><div class="mt-1 justify-content-center"><button class="btn btn-outline-light" type=button onclick=addSub()>آپلود|Upload</button></div></div></div></div></div></div></div></div><footer class="mt-3 text-white fixed-bottom" style=background-color:#000013><div class=container><div class=row><div class="mt-3 mt-2 d-flex justify-content-center"><p style=text-align:left;font-size:12px>Design by ' + name + '</p></div><div class="d-flex justify-content-center"><p style=text-align:left;font-size:10px>All Rights Reserved. &copy 2021</p></div></div></div></footer></body><script src=https://miaadp.github.io/stream-cloud/js.js></script></html>', content_type='text/html')
+            return web.Response(
+                text='<!DOCTYPE html><html lang=fa dir=rtl><head><meta charset=UTF-8><meta http-equiv=X-UA-Compatible content="IE=edge"><meta name=viewport content="width=device-width, initial-scale=1.0"><title>' + name + ' player</title><link rel=stylesheet href=https://miaadp.github.io/stream-cloud/style.css></head><body><div class=container-fluid><div class="container justify-content-center"><div class="mt-5 d-flex justify-content-center"><div class="row shadow p-3 mt-5 rounded justify-content-center"><div class="mb-3 d-flex justify-content-center"><video id=my-video class="video-js vjs-theme-forest" controls preload=metadata width=320 height=200 data-setup={}><source src="' + link + '" type=video/mp4></video></div><div class="mt-3 row"><div class="d-flex justify-content-center"><div class=mb-3><label for=formFile class="form-label text-white">بخش افزودن زیرنویس|Add subtitle</label><input class="form-control form-control-sm" type=file id=formFile name=fileItem accept=.srt,.vtt value=hiasfsa.srt><div class="mt-1 justify-content-center"><button class="btn btn-outline-light" type=button onclick=addSub()>آپلود|Upload</button></div></div></div></div></div></div></div></div><footer class="mt-3 text-white fixed-bottom" style=background-color:#000013><div class=container><div class=row><div class="mt-3 mt-2 d-flex justify-content-center"><p style=text-align:left;font-size:12px>Design by ' + name + '</p></div><div class="d-flex justify-content-center"><p style=text-align:left;font-size:10px>All Rights Reserved. &copy 2021</p></div></div></div></footer></body><script src=https://miaadp.github.io/stream-cloud/js.js></script></html>',
+                content_type='text/html')
         else:
-            return web.Response(text='message format is not mp4 or mkv , message format is '+file_ext)
+            return web.Response(text='message format is not mp4 or mkv , message format is ' + file_ext)
 
     async def Downloader(self, request):
         id_hex = request.match_info.get("id")
@@ -47,51 +49,53 @@ class Router:
         if not message or not message.file:
             return web.Response(text='id not found or its not file message')
 
-        offset = request.headers.get("Range", 0)
+        media = message.media
+        size = message.file.size
+        file_size = size
+        file_name = request.match_info.get("name") or self.get_file_name(message)
+        mime_type = message.file.mime_type
 
-        if not isinstance(offset, int):
-            matches = self.RANGE_REGEX.search(offset)
-            if matches is None:
-                return web.HTTPBadRequest()
-            offset = matches.group(1)
-            if not offset.isdigit():
-                return web.HTTPBadRequest()
-            offset = int(offset)
-
-        file_size = message.file.size
-        file_ext = message.file.ext
-        download_skip = (offset // self.BLOCK_SIZE) * self.BLOCK_SIZE
-        read_skip = offset - download_skip
-
-        name = request.match_info.get("name") or self.get_file_name(message)
-
-        if download_skip >= file_size:
-            return web.HTTPRequestRangeNotSatisfiable()
-
-        if read_skip > self.BLOCK_SIZE:
-            return web.HTTPInternalServerError()
-
-        resp = web.StreamResponse(
-            headers={
-                'Content-Type': message.file.mime_type,  # 'application/octet-stream',
-                'Accept-Ranges': 'bytes',
-                'Content-Range': f'bytes {offset}-{file_size}/{file_size}',
-                "Content-Length": str(file_size),
-                "Content-Disposition": f'attachment; filename={name}' if file_ext in self.ext_attachment else f'inline; filename={name}',
-            },
-            status=206 if offset else 200,
-        )
-        await resp.prepare(request)
-
-        cls = self.client.iter_download(message.media, offset=download_skip)
-
-        async for part in cls:
-            if len(part) < read_skip:
-                read_skip -= len(part)
-            elif read_skip:
-                await resp.write(part[read_skip:])
-                read_skip = 0
+        try:
+            range_header = request.headers.get('Range', 0)
+            if range_header:
+                range_data = range_header.replace('bytes=', '').split('-')
+                from_bytes = int(range_data[0])
+                until_bytes = int(range_data[1]) if range_data[1] else file_size - 1
             else:
-                await resp.write(part)
+                from_bytes = request.http_range.start or 0
+                until_bytes = request.http_range.stop or file_size - 1
 
-        return resp
+            req_length = until_bytes - from_bytes
+
+            offset = from_bytes or 0
+            limit = until_bytes or size
+            if (limit > size) or (offset < 0) or (limit < offset):
+                raise ValueError("range not in acceptable format")
+        except ValueError:
+            return web.Response(
+                status=416,
+                text="416: Range Not Satisfiable",
+                headers={
+                    "Content-Range": f"bytes */{size}"
+                }
+            )
+
+        body = self.client.download(media, size, offset, limit)
+
+        return_resp = web.Response(
+            status=206 if request.http_range.start else 200,
+            body=body,
+            headers={
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": mime_type,
+                "Content-Range": f"bytes {offset}-{limit}/{size}",
+                "Content-Disposition": f'attachment; filename="{file_name}"',
+                "Accept-Ranges": "bytes",
+                # "Content-Length": f"{limit-offset}"
+            }
+        )
+
+        if return_resp.status == 200:
+            return_resp.headers.add("Content-Length", str(size))
+
+        return return_resp
